b766500fe1faae257e47f3c81d88db134c3b71f8
Insoo Baek <baikinsoo95@gmail.com>
2023-07-12 22:20:39 +0900

    Initial commit

---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,32 @@
+package hellojpa;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+//관례상 @Table로 테이블 명을 작성해주지 않으면 클래스명이 테이블 명이 된다.
+public class Member {
+
+    @Id
+    private Long id;
+
+    //@Colum을 통해서 Colum 이름도 테이블에 작성된 이름으로 작성할 수 있다.
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
